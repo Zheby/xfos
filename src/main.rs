b8aba42f2b6c6bc40a0e98b6a1c4b1f8ebbc5b0d
Zheby <256814605@qq.com>
2023-03-19 13:23:55 +0800

    0.0.2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+#![feature(decl_macro)]
 #![no_std]
 #![no_main]
 #[allow(special_module_name,non_snake_case)]
 mod cores;
+pub mod vga;
+#[no_mangle]
 pub extern "C" fn _start() ->! {
+    println!("hello word");
+    loop {}
+
+}
+
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
